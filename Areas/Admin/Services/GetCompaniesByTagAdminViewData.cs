bbfad7fbb6141399c433efb2558cfd162de528c6
Diego Lucena <diegojlucena@gmail.com>
2016-01-06 13:44:13 -0200

    widget for listing companies by tag

---
+++ Areas/Admin/Services/GetCompaniesByTagAdminViewData.cs
@@ -0,0 +1,41 @@
+using System.Web.Mvc;
+using MrCMS.Entities.Documents;
+using MrCMS.Helpers;
+using MrCMS.Web.Apps.Companies.Pages;
+using MrCMS.Web.Apps.Companies.Widgets;
+using MrCMS.Web.Areas.Admin.Services;
+using MrCMS.Website;
+using NHibernate;
+
+namespace MrCMS.Web.Apps.Companies.Areas.Admin.Services
+{
+    public class GetCompaniesByTagAdminViewData : BaseAssignWidgetAdminViewData<CompaniesByTag>
+    {
+        private readonly ISession _session;
+
+        public GetCompaniesByTagAdminViewData(ISession session)
+        {
+            _session = session;
+        }
+
+        public override void AssignViewData(CompaniesByTag widget, ViewDataDictionary viewData)
+        {
+            viewData["companiesList"] = _session.QueryOver<CompanyList>()
+                .Where(article => article.PublishOn != null && article.PublishOn <= CurrentRequestData.Now)
+                .Cacheable()
+                .List()
+                .BuildSelectItemList(item => item.Name,
+                    item => item.Id.ToString(),
+                    emptyItemText: "Please select a company list");
+
+
+
+            viewData["tags"] = _session.QueryOver<Tag>()
+                .Cacheable()
+                .List()
+                .BuildSelectItemList(item => item.Name,
+                    item => item.Id.ToString(),
+                    emptyItemText: "Please select a tag");
+        }
+    }
+}
---
+++ Widgets/CompaniesByTag.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using MrCMS.Entities.Documents;
+using MrCMS.Entities.Widget;
+using MrCMS.Web.Apps.Companies.Pages;
+using MrCMS.Website;
+
+namespace MrCMS.Web.Apps.Companies.Widgets
+{
+    [OutputCacheable(PerPage = true)]
+    public class CompaniesByTag : Widget
+    {
+        public virtual CompanyList CompanList { get; set; }
+
+        [DisplayName("Show Name As Title")]
+        public virtual bool ShowNameAsTitle { get; set; }
+
+        public virtual Tag Tag { get; set; }
+    }
+}
